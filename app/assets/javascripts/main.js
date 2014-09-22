f01bfa349eaf12a7c5f342d28777c99d572f467e
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-22 12:01:07 -0500

    post not get

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	console.log("ready");
-	$("#submit_btn").on("click", function(e){
+$("#submit_btn").on("click", function(e){
 		e.preventDefault();
 		var keys = {
 			YOUR_API_KEY: '751n3s1mdd0pu7',
@@ -8,23 +8,13 @@ $(document).ready(function(){
 			STATE: '1937rbfiek3ubgfk2irRwhETs',
 			YOUR_REDIRECT_URI: 'http://localhost:3000'
 		};
-		var request = $.ajax(
-			{url: "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id="+keys.YOUR_API_KEY+"&scope="+keys.SCOPE+"&state="+keys.STATE+"&redirect_uri="+keys.YOUR_REDIRECT_URI,
-				type: 'GET',
-				jsonp: 'callback',
-				dataType: 'jsonp',
-				success: function (data, status, error) {
-		      console.log('success', data);
-		    }
-			});
-
+		var request = $.post("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id="+keys.YOUR_API_KEY+"&scope="+keys.SCOPE+"&state="+keys.STATE+"&redirect_uri="+keys.YOUR_REDIRECT_URI);
 		// request = $.get("fizzbuzz", {input: $("#txtArea").val()})
 		// request.done(function(data){
 		// 	console.log(data);
 		// 	$("#returnedText").html('<p>'+data+'</p>')
 		// 	Style_fizzbuzz();
 		// })
-		
 	})
 	function Style_fizzbuzz(){
 		var fizzbuzzed = $('#returnedText').text().replace(/Buzzword/g,"<span class = 'buzz'>Buzzword</span>").replace(/Fizz/g,"<span class = 'fizz'>Fizz</span>");
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	before_filter :set_access
-
-	def set_access
-		p "set access"
-	  headers["Access-Control-Allow-Origin"] = "*"
-	  headers['Access-Control-Allow-Methods'] = ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE']
-	end
 end
