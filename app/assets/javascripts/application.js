68930ff743f4fe55d903da764acadac107132634
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-22 00:03:30 -0500

    application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	console.log("ready");
+	$("#submit_btn").on("click", function(e){
+		e.preventDefault();
+		request = $.get("fizzbuzz", {input: $("#txtArea").val()})
+		request.done(function(data){
+			console.log(data);
+			$("#returnedText").html('<p>'+data+'</p>')
+		})
+		
+	})
+});
