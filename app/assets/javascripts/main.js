7e0ffe029d032d10ddc01cbae5a521cf5406af67
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-22 10:00:26 -0500

    assets

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,13 @@ $(document).ready(function(){
 		request.done(function(data){
 			console.log(data);
 			$("#returnedText").html('<p>'+data+'</p>')
+			Style_fizzbuzz();
 		})
 		
 	})
+	function Style_fizzbuzz(){
+		var fizzbuzzed = $('#returnedText').text().replace(/Buzzword/g,"<span class = 'buzz'>Buzzword</span>").replace(/Fizz/g,"<span class = 'fizz'>Fizz</span>");
+		$('#returnedText').html(fizzbuzzed);
+	}
+	
 });
