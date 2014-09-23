6811e65038efe4cfa8bab2da0d22122eb4a8a794
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-22 23:23:30 -0500

    controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,26 @@
+require 'pry'
 class WelcomeController < ApplicationController
   def index
     p "IN INDEX"
   end
+  
+  def oauth
+    response = Oauth.signin
+    p "*"*100
+    if request.xhr?
+      render :json => response[:url].to_json
+    end
+  end
 
   def fizzbuzz
-
-    p "fizzbuzz!"*25
+    access_token = Oauth.get_accessToken(params[:code])
+    profile = Oauth.get_profile(access_token)
 		# " I am responsible. creative"
-  	response = Fizzbuzz.fizzbuzzed(params[:input])
+  	@response = Fizzbuzz.fizzbuzzed(profile["summary"])
   	if request.xhr?
-  		render :json => response
-  	end
+  		render :json => @response.to_json
+  	else
+      render :json => response.to_json
+    end
   end
 end
